183cad157dd8a39f6bfc18fbce81928671e27fb9
jasonDeng <dengjason320@gmail.com>
2026-07-19 21:06:16 +0800

    Day1 Python Basics

---
+++ day01/day01.py.py
@@ -0,0 +1,57 @@
+"""
+任务1
+"""
+print("Hello Python")
+print("你好，我叫Jason")
+print("欢迎来到Python世界")
+"""
+任务2
+"""
+# 定义一个变量，用来记录名字
+name = "Jason"
+# 定义一个变量，用来记录年龄
+age = 24
+# 定义一个变量，用来记录专业
+major = "机械设计制造及其自动化"
+#定义一个变量，用来记录学校
+school = "华侨大学"
+#定义一个变量，用来记录城市
+city = "厦门"
+#定义一个变量，用来记录目标
+goal = "AI视觉检测工程师"
+#通过print语句，输出名字、年龄和专业
+print("名字：",name)
+print("年龄：",age)
+print("专业：",major)
+print("学校：",school)
+print("城市：",city)
+print("目标：",goal)
+"""
+任务三
+"""
+# 定义一个变量，记录名字这个字符串字面量
+name = "Jason"
+# 定义一个变量，记录年龄这个整数字面量
+age = 24
+# 定义一个变量，记录身高这个浮点型字面量
+height = 1.75
+#定义一个变量，记录一个布尔字面量
+is_student = False
+#通过print语句输出各个数据的类型
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+"""
+任务四
+"""
+#定义两个变量，分别记录整数字面量20和5
+a = 20
+b = 5
+#通过print语句输出二者的和、差、积和商
+print("和：",a + b)
+print("差：",a - b)
+print("积：",a * b)
+print("商：",a / b)
+print("取整",a // b)
+print("取余",a % b)
